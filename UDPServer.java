31133870bd6145f8d528b970cd075db94fef344f
Jim Fitzgerald <dumples5@gmail.com>
2012-09-23 14:08:43 -0400

    changed some file names and updated UDPServer

--- UDPServer.java
+++ UDPServer.java
@@ -6,7 +6,7 @@ class UDPServer {
     { 
   
 	  // The chosen port of the server is: 21212 //
-      DatagramSocket serverSocket = new DatagramSocket(21212); 
+      DatagramSocket serverSocket = new DatagramSocket(21212);
   
       byte[] receiveData = new byte[1024]; 
       byte[] sendData  = new byte[1024]; 
@@ -15,24 +15,31 @@ class UDPServer {
         { 
   
           DatagramPacket receivePacket = 
-             new DatagramPacket(receiveData, receiveData.length); 
+             new DatagramPacket(receiveData, receiveData.length);
+          System.out.println("Waiting to recieve a packet...");
            serverSocket.receive(receivePacket); 
 		  String sentence = new String(receivePacket.getData()); 
   
           InetAddress IPAddress = receivePacket.getAddress(); 
   
           int port = receivePacket.getPort(); 
-  
-          //Convert 'sentence' toUpperCase(); //
+
+          System.out.println("A packet of: <" + sentence + "> has been received from <" + IPAddress + "> through port <" + port);
 
 		  // 'Pack' the sentence into 'sendData', echo to verify, make a packet of it, and send it back via the 'port'//
           File sendFile = new File(sentence);
           FileInputStream fileStream = new FileInputStream(sendFile);
-          fileStream.read(sendData);
+          int bytesRead = fileStream.read(sendData, 0, sendData.length);
+
+          do{
+              System.out.println("There are " + fileStream.available() + "remaining bytes");
+            fileStream.read(sendData);
+            bytesRead = fileStream.read(sendData);
+          }while(bytesRead!=-1);
 
           //sendData = IOUtils.toByteArray(filestream);
- 
-          DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+          System.out.println(sendData);
+          DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 2121);
           System.out.println("Datagram packed...Sending...");
           serverSocket.send(sendPacket); 
           System.out.println("Datagram sent");
