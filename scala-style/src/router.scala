9745b9b48d9d3ea16ffc1817c1e25c43c0d390ac
Jim Fitzgerald <dumples5@gmail.com>
2012-10-18 15:55:03 -0400

    router development continues...look at TODO

--- scala-style/src/router.scala
+++ scala-style/src/router.scala
@@ -14,16 +14,36 @@ object router {
   def main(args: List[String]) = {
     require(args < 6, "Must have 5 or less arguments")
 
-    val incomingSocket = new ServerSocket(2222)
+    val servSock = new ServerSocket(2222)
+    val clientSock = servSock.accept()
+    var start = Int
     val ipList = buildRoutingTable(args, List[(String, String)])
 
     while(true){
-
+      println("Waiting to receive message...")
+      buildData(clientSock, ipList)
     }
   }
 
   def buildRoutingTable(args: List[String], list: List[(String, String)]): List[(String, String)] = {
     if (args.isEmpty) list
     else {list ::: (args.head, "2222"); buildRoutingTable(args.tail, list)}
+  }
+
+  def buildData(clientSock: Socket, ipList: List[(String, String)]) {
+    def receiveMessage(): List[String] = {
+      val receiveData = Array[Byte]
+      val inFromClient = DataInputStream(clientSock.getInputStream)
+      inFromClient.read(receiveData)
+      var message = new String(receiveData)
+      message = message.trim
+      val messages: List[String] = message.split(":");
+    }
+
+    val start = System.currentTimeMillis()
+    //TODO: Fix this, probably need to be a recursive function
+    for(i <- receiveMessage()){
+      if (ipList(i, 0) == )
+    }
   }
 }
